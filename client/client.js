f157b382b2e4cf9bb870f41af036d0327c04e9dc
Arturo Cordoba <arturocv16@gmail.com>
2021-05-04 00:09:24 -0600

    Comentarios adicionales en el cliente

--- client/client.js
+++ client/client.js
@@ -1,8 +1,11 @@
+// Ruta del contrato
 const PROTO_PATH = "./pokemon.proto";
 
+// Importar bibliotecas
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 
+// Cargar contrato de servicio
 var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
     keepCase: true,
     longs: String,
@@ -10,10 +13,14 @@ var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
     arrays: true
 });
 
+// Registrar contrato en gRPC
 const PokemonService = grpc.loadPackageDefinition(packageDefinition).PokemonService;
+
+// Crear cliente
 const client = new PokemonService(
     'localhost:30000',
     grpc.credentials.createInsecure()
 );
 
+// Exportar cliente
 module.exports = client;
